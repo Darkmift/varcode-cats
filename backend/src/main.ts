039bb99ae08490be261ad769b3a559e07c0a60cd
Darkmift <darkmift@gmail.com>
2024-03-13 18:12:56 +0200

    Add configuration for environment variables

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  try {
+    await app.listen(configService.get('PORT'));
+    Logger.log(`Server started on port ${configService.get('PORT')}`);
+  } catch (error) {
+    Logger.error(`Failed to start the app: ${error}`);
+  }
 }
 bootstrap();
