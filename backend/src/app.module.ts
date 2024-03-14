45f8565a9f2e7cd31452ed4c7a8607d62d5e2cba
Darkmift <darkmift@gmail.com>
2024-03-14 18:48:21 +0200

    Update app.module.ts with new imports and configuration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,16 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
-import { validate } from './config/env.validation';
+import {
+  RootConfig,
+  validate,
+} from './config/env.validation';
+import { AuthModule } from './auth/auth.module';
+import { CatsModule } from './cats/cats.module';
+import { TypedConfigModule, dotenvLoader } from 'nest-typed-config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true, 
+    TypedConfigModule.forRoot({
+      isGlobal: true,
+      schema: RootConfig,
       validate,
-      envFilePath: '.env', 
+      load: dotenvLoader({
+        envFilePath: '.env',
+      }),
     }),
+    AuthModule,
+    CatsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
