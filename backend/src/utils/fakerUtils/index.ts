a635e8fabd1488d9f96c07020c6b9191e5e42cb2
Darkmift <darkmift@gmail.com>
2024-03-16 22:03:19 +0200

    Add fakerUtils module for generating random users and cats

---
+++ backend/src/utils/fakerUtils/index.ts
@@ -0,0 +1,102 @@
+import { IUser } from '@/auth/auth.types';
+import { faker } from '@faker-js/faker';
+import { hashString } from '../bcrypt';
+import { ICat } from '@/cats/types/cats.type';
+
+const catsImages = [
+  {
+    id: '2bl',
+    url: 'https://cdn2.thecatapi.com/images/2bl.jpg',
+    width: 334,
+    height: 500,
+  },
+  {
+    id: '3kk',
+    url: 'https://cdn2.thecatapi.com/images/3kk.jpg',
+    width: 500,
+    height: 342,
+  },
+  {
+    id: '59d',
+    url: 'https://cdn2.thecatapi.com/images/59d.jpg',
+    width: 500,
+    height: 375,
+  },
+  {
+    id: 'aco',
+    url: 'https://cdn2.thecatapi.com/images/aco.jpg',
+    width: 506,
+    height: 380,
+  },
+  {
+    id: 'bjm',
+    url: 'https://cdn2.thecatapi.com/images/bjm.jpg',
+    width: 500,
+    height: 354,
+  },
+  {
+    id: 'MTYzOTU4MA',
+    url: 'https://cdn2.thecatapi.com/images/MTYzOTU4MA.jpg',
+    width: 500,
+    height: 327,
+  },
+  {
+    id: 'MTc4MTg0OA',
+    url: 'https://cdn2.thecatapi.com/images/MTc4MTg0OA.gif',
+    width: 500,
+    height: 246,
+  },
+  {
+    id: 'MTc5NTYxNg',
+    url: 'https://cdn2.thecatapi.com/images/MTc5NTYxNg.jpg',
+    width: 240,
+    height: 320,
+  },
+  {
+    id: 'MjA3OTE1MA',
+    url: 'https://cdn2.thecatapi.com/images/MjA3OTE1MA.jpg',
+    width: 506,
+    height: 556,
+  },
+  {
+    id: 'tnxX4IOst',
+    url: 'https://cdn2.thecatapi.com/images/tnxX4IOst.jpg',
+    width: 1080,
+    height: 1350,
+  },
+];
+
+function makeNumber(min, max) {
+  return faker.number.bigInt({ min, max }) as unknown as number;
+}
+
+//pick a random image from the list
+function getRandomImage() {
+  const randomIndex = Math.floor(Math.random() * catsImages.length);
+  return catsImages[randomIndex];
+}
+
+// a function that makes a random IUser
+export async function createRandomUser(): Promise<IUser> {
+  const password = await hashString(faker.internet.password());
+  return {
+    first_name: faker.person.firstName(),
+    last_name: faker.person.lastName(),
+    username: faker.internet.userName(),
+    password,
+  };
+}
+
+// a function that makes a random ICat
+export function createRandomCat(): ICat {
+  return {
+    name: faker.animal.cat(),
+    birthday: faker.date.birthdate(),
+    location: faker.location.city(),
+    favorite_food: faker.animal.cat(),
+    fur_color: faker.color.human(),
+    height: makeNumber(10, 50),
+    weight: makeNumber(1, 10),
+    image_url: getRandomImage().url,
+  };
+}
