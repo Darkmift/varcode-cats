7b35cf017e223ff4999ba866ce8ad3e3f9f1b70a
Darkmift <darkmift@gmail.com>
2024-03-16 22:43:24 +0200

    Add JWT module to AuthModule

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -3,11 +3,23 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Admin, User } from './user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { TypedConfigModule } from 'nest-typed-config';
+import { RootConfig } from '@/config/env.validation';
 
 @Module({
   providers: [AuthService],
   controllers: [AuthController],
   exports: [AuthService],
-  imports: [TypeOrmModule.forFeature([User, Admin])],
+  imports: [
+    TypeOrmModule.forFeature([User, Admin]),
+    JwtModule.registerAsync({
+      imports: [TypedConfigModule],
+      useFactory: async (rootConfig: RootConfig) => ({
+        secret: rootConfig.JWT_SECRET,
+      }),
+      inject: [RootConfig],
+    }),
+  ],
 })
 export class AuthModule {}
