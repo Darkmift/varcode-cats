1b3d7ff9b03b5f61daa66e89c36f9dbce320def4
Darkmift <darkmift@gmail.com>
2024-03-16 10:07:10 +0200

    Update Swagger configuration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { RootConfig } from './config/env.validation';
-import { initSwagger } from './swagger/config';
+import { initSwagger } from './utils/swagger/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
