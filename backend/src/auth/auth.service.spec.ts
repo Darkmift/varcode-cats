bb474420483492e5bebc806920280570c8b83def
Darkmift <darkmift@gmail.com>
2024-03-16 22:44:00 +0200

    Refactor auth.service.ts to include user and admin creation, login, and token generation

--- backend/src/auth/auth.service.spec.ts
+++ backend/src/auth/auth.service.spec.ts
@@ -1,46 +1,214 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { AuthService } from './auth.service';
-import { Logger } from '@nestjs/common';
-import { ILoginParams } from './auth.types';
+import { User, Admin } from './user.entity';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { TypedConfigModule, dotenvLoader } from 'nest-typed-config';
+import { RootConfig, validate } from '@/config/env.validation';
+import { Cat, CatVote } from '@/cats/cats.entity';
+import { IAdmin } from './auth.types';
+
+jest.setTimeout(30000);
 
 describe('AuthService', () => {
   let service: AuthService;
-  let logSpy: jest.SpyInstance;
+  let jwtService: JwtService;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        JwtModule.registerAsync({
+          imports: [TypedConfigModule],
+          useFactory: async (rootConfig: RootConfig) => ({
+            secret: rootConfig.JWT_SECRET,
+          }),
+          inject: [RootConfig],
+        }),
+        TypedConfigModule.forRoot({
+          isGlobal: true,
+          schema: RootConfig,
+          validate,
+          load: dotenvLoader({
+            envFilePath: '.env.test.local',
+          }),
+        }),
+        TypeOrmModule.forRootAsync({
+          imports: [TypedConfigModule],
+          useFactory: async (rootConfig: RootConfig) => {
+            const {
+              NODE_ENV,
+              POSTGRES_DB,
+              POSTGRES_HOST,
+              POSTGRES_PORT,
+              POSTGRES_USER,
+              POSTGRES_PASSWORD,
+            } = rootConfig;
+
+            const baseOptions = {
+              type: 'postgres',
+              entities: [Cat, CatVote, User, Admin],
+              synchronize: true,
+            };
+
+            const options = {
+              host: POSTGRES_HOST,
+              port: POSTGRES_PORT,
+              username: POSTGRES_USER,
+              password: POSTGRES_PASSWORD,
+              database: POSTGRES_DB,
+            };
+            return { ...options, ...baseOptions } as TypeOrmModuleOptions;
+          },
+          inject: [RootConfig],
+        }),
+        TypeOrmModule.forFeature([Cat, CatVote, User, Admin]),
+      ],
       providers: [AuthService],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
-
-    logSpy = jest.spyOn(Logger, 'log').mockImplementation(() => {});
+    jwtService = module.get<JwtService>(JwtService);
   });
 
-  afterEach(() => {
-    logSpy.mockRestore();
+  afterAll(async () => {
+    await service['userRepository'].query(
+      'TRUNCATE TABLE admin, "user",cat, cat_vote CASCADE;',
+    );
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('createUser', () => {
+    it('should create and return a user', async () => {
+      const userParams = {
+        username: 'testUser',
+        password: 'password',
+        first_name: 'Test',
+        last_name: 'User',
+      };
+
+      const createdUser = await service.createUser(structuredClone(userParams));
+      expect(createdUser).toBeDefined();
+      expect(createdUser.username).toEqual(userParams.username);
+      // Verify password is hashed
+      expect(createdUser.password).not.toEqual(userParams.password);
+    });
   });
 
-  it('login should log the user in and return login result', async () => {
-    const mockUser: ILoginParams = {
-      username: 'testUser',
-      password: 'testPass',
-    };
+  describe('getUserById', () => {
+    let existingUserId: string;
 
-    const loginResult = await service.login(mockUser);
+    beforeAll(async () => {
+      const newUser = await service.createUser({
+        username: 'testUserForGetById',
+        password: 'password',
+        first_name: 'Test',
+        last_name: 'UserGetById',
+      });
+      existingUserId = newUser.id;
+    });
 
-    expect(logSpy).toHaveBeenCalledWith('This action logs a user in', mockUser);
+    it('should return a user by ID', async () => {
+      const user = await service.getUserById(existingUserId);
+      expect(user).toBeDefined();
+      expect(user.id).toEqual(existingUserId);
+    });
 
-    expect(loginResult).toEqual({
-      token: expect.any(String),
-      role: 'user',
-      username: mockUser.username,
-      lastname: 'lastname',
-      firstname: 'firstname',
+    it('should return null for a non-existing ID', async () => {
+      const nonExistingId = 'b539dc9b-df17-441f-b863-eeeac0844a95';
+      const user = await service.getUserById(nonExistingId);
+      expect(user).toBeNull();
+    });
+  });
+
+  describe('createAdmin', () => {
+    it('should create and return an admin', async () => {
+      const adminParams: IAdmin = {
+        username: 'testAdmin',
+        password: 'adminPassword',
+        first_name: 'Admin',
+        last_name: 'User',
+        email: 'admin@test.com',
+        accessLevel: 'admin',
+      };
+
+      const createdAdmin = await service.createAdmin(
+        structuredClone(adminParams),
+      );
+      expect(createdAdmin).toBeDefined();
+      expect(createdAdmin.username).toEqual(adminParams.username);
+      // Verify password is hashed
+      expect(createdAdmin.password).not.toEqual(adminParams.password);
+    });
+  });
+
+  describe('login', () => {
+    let testUserId: string;
+
+    beforeAll(async () => {
+      const user = await service.createUser({
+        username: 'loginTestUser',
+        password: 'testPass',
+        first_name: 'Login',
+        last_name: 'Test',
+      });
+      testUserId = user.id;
+    });
+
+    it('should allow a user to log in and return a valid token', async () => {
+      const loginResult = await service.login({
+        username: 'loginTestUser',
+        password: 'testPass', // You need to adjust the logic to correctly validate the password
+      });
+
+      expect(loginResult).toBeDefined();
+      expect(loginResult.token).toBeDefined();
+      expect(loginResult.username).toBe('loginTestUser');
+      expect(loginResult.role).toBe('user');
+    });
+
+    it('should return null for invalid login credentials', async () => {
+      const loginResult = await service.login({
+        username: 'nonexistentUser',
+        password: 'wrongPass',
+      });
+
+      expect(loginResult).toBeNull();
+    });
+  });
+
+  describe('loginAdmin', () => {
+    let testAdminId: string;
+
+    beforeAll(async () => {
+      const admin = await service.createAdmin({
+        username: 'loginTestAdmin',
+        password: 'adminTestPass',
+        first_name: 'Admin',
+        last_name: 'LoginTest',
+        email: 'admintest@test.com',
+        accessLevel: 'admin',
+      });
+      testAdminId = admin.id;
+    });
+
+    it('should allow an admin to log in and return a valid token', async () => {
+      const loginResult = await service.loginAdmin({
+        username: 'loginTestAdmin',
+        password: 'adminTestPass', // Adjust password validation logic accordingly
+      });
+
+      expect(loginResult).toBeDefined();
+      expect(loginResult.token).toBeDefined();
+      expect(loginResult.username).toBe('loginTestAdmin');
+      expect(loginResult.role).toBe('admin');
+    });
+
+    it('should return null for invalid admin login credentials', async () => {
+      const loginResult = await service.loginAdmin({
+        username: 'nonexistentAdmin',
+        password: 'wrongPass',
+      });
+
+      expect(loginResult).toBeNull();
     });
   });
 });
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,17 +1,88 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { ILoginParams } from './auth.types';
-import { LoginResultDto } from './dto/auth.index';
+import { IAdmin, ILoginParams, ILoginResult, IUser } from './auth.types';
+import { Repository } from 'typeorm';
+import { Admin, User } from './user.entity';
+import { compareStringToHash, hashString } from '@/utils/bcrypt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  async login(user: ILoginParams): Promise<LoginResultDto> {
-    Logger.log('This action logs a user in', user);
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+    @InjectRepository(Admin)
+    private adminRepository: Repository<Admin>,
+    private jwtService: JwtService,
+  ) {}
+
+  //a fn that creates a User
+  async createUser(user: IUser): Promise<User> {
+    user.password = await hashString(user.password);
+    return this.userRepository.save(user);
+  }
+
+  //a fn that creates a admin
+  async createAdmin(adminUser: IAdmin): Promise<Admin> {
+    adminUser.password = await hashString(adminUser.password);
+    return this.adminRepository.save(adminUser);
+  }
+
+  async getUserById(id: string): Promise<User> {
+    return this.userRepository.findOneBy({ id });
+  }
+
+  async login(user: ILoginParams): Promise<ILoginResult> {
+    const userFound = await this.userRepository.findOne({
+      where: { username: user.username },
+    });
+    if (!userFound) {
+      return null;
+    }
+
+    const isPasswordValid = await compareStringToHash(
+      user.password,
+      userFound.password,
+    );
+    if (!isPasswordValid) {
+      return null;
+    }
+    const payload = { username: user.username, sub: userFound.id };
+    const token = await this.jwtService.signAsync(payload);
+
     return {
-      token: 'token',
+      token,
+      firstname: userFound.first_name,
+      lastname: userFound.last_name,
+      username: userFound.username,
       role: 'user',
-      username: user.username,
-      lastname: 'lastname',
-      firstname: 'firstname',
+    };
+  }
+
+  async loginAdmin(admin: ILoginParams): Promise<ILoginResult> {
+    const adminFound = await this.adminRepository.findOne({
+      where: { username: admin.username },
+    });
+    if (!adminFound) {
+      return null;
+    }
+
+    const isPasswordValid = await compareStringToHash(
+      admin.password,
+      adminFound.password,
+    );
+    if (!isPasswordValid) {
+      return null;
+    }
+    const payload = { username: admin.username, sub: adminFound.id };
+    const token = await this.jwtService.signAsync(payload);
+
+    return {
+      token,
+      firstname: adminFound.first_name,
+      lastname: adminFound.last_name,
+      username: adminFound.username,
+      role: 'admin',
     };
   }
 }
