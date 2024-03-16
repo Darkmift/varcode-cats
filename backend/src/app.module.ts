dac8276d94262041a60425f4cb76cf27f27df0d8
Darkmift <darkmift@gmail.com>
2024-03-16 22:04:12 +0200

    Refactor app.module.ts to include TypeORM configuration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,14 +1,12 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import {
-  RootConfig,
-  validate,
-} from './config/env.validation';
+import { RootConfig, validate } from './config/env.validation';
 import { AuthModule } from './auth/auth.module';
 import { CatsModule } from './cats/cats.module';
 import { TypedConfigModule, dotenvLoader } from 'nest-typed-config';
 import { LoggingModule } from './logging/logging.module';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -19,6 +17,37 @@ import { LoggingModule } from './logging/logging.module';
       load: dotenvLoader({
         envFilePath: '.env',
       }),
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [TypedConfigModule, LoggingModule],
+      useFactory: async (rootConfig: RootConfig) => {
+        const {
+          NODE_ENV,
+          POSTGRES_DB,
+          POSTGRES_HOST,
+          POSTGRES_PORT,
+          POSTGRES_USER,
+          POSTGRES_PASSWORD,
+        } = rootConfig;
+
+        const path = __dirname + '/**/*.entity{.ts,.js}';
+        const baseOptions = {
+          type: 'postgres',
+          entities: [path],
+          synchronize: true,
+        };
+
+        const options = {
+          host: POSTGRES_HOST,
+          port: POSTGRES_PORT,
+          username: POSTGRES_USER,
+          password: POSTGRES_PASSWORD,
+          database: POSTGRES_DB,
+        };
+        Logger.log('initializing typeorm', { NODE_ENV });
+        return { ...options, ...baseOptions } as TypeOrmModuleOptions;
+      },
+      inject: [RootConfig],
     }),
     AuthModule,
     CatsModule,
