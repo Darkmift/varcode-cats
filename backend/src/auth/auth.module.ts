4ed0609a10341757219f90b6fd51cddc13ac5d55
Darkmift <darkmift@gmail.com>
2024-03-16 22:04:22 +0200

    Add TypeOrmModule and User entity to AuthModule and Cat entity, CatVote entity, and User entity to CatsModule

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Admin, User } from './user.entity';
 
 @Module({
   providers: [AuthService],
-  controllers: [AuthController]
+  controllers: [AuthController],
+  exports: [AuthService],
+  imports: [TypeOrmModule.forFeature([User, Admin])],
 })
 export class AuthModule {}
--- backend/src/cats/cats.module.ts
+++ backend/src/cats/cats.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { CatsController } from './cats.controller';
+import { AuthModule } from '@/auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Cat, CatVote } from './cats.entity';
+import { User } from '@/auth/user.entity';
 
 @Module({
   providers: [CatsService],
-  controllers: [CatsController]
+  controllers: [CatsController],
+  imports: [AuthModule, TypeOrmModule.forFeature([Cat, CatVote, User])],
 })
 export class CatsModule {}
