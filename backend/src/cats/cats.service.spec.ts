75fa3c18ef4abca397d150a823259441e578bf1f
Darkmift <darkmift@gmail.com>
2024-03-16 22:12:07 +0200

    Refactor CatsService and add unit tests

--- backend/src/cats/cats.service.spec.ts
+++ backend/src/cats/cats.service.spec.ts
@@ -1,74 +1,163 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { CatsService, mockCats } from './cats.service';
-import {
-  CatDTO,
-  PaginationParamsDTO,
-  PaginationResultDTO,
-} from './dto/cats.index';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { CatsService } from './cats.service';
+import { Cat, CatVote } from './cats.entity';
+import { TypedConfigModule, dotenvLoader } from 'nest-typed-config';
+import { RootConfig, validate } from '@/config/env.validation';
+import { Admin, User } from '@/auth/user.entity';
+import { AuthModule } from '@/auth/auth.module';
+
+jest.setTimeout(30000);
+
+jest.mock('@/utils/datadog/datadog', () => {
+  return {
+    DatadogLogger: jest.fn().mockImplementation(() => ({
+      log: jest.fn(),
+      error: jest.fn(),
+      warn: jest.fn(),
+      debug: jest.fn(),
+      verbose: jest.fn(),
+    })),
+  };
+});
 
 describe('CatsService', () => {
   let service: CatsService;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        AuthModule,
+        TypedConfigModule.forRoot({
+          isGlobal: true,
+          schema: RootConfig,
+          validate,
+          load: dotenvLoader({
+            envFilePath: '.env.test.local',
+          }),
+        }),
+        TypeOrmModule.forRootAsync({
+          imports: [TypedConfigModule],
+          useFactory: async (rootConfig: RootConfig) => {
+            const {
+              NODE_ENV,
+              POSTGRES_DB,
+              POSTGRES_HOST,
+              POSTGRES_PORT,
+              POSTGRES_USER,
+              POSTGRES_PASSWORD,
+            } = rootConfig;
+
+            const baseOptions = {
+              type: 'postgres',
+              entities: [Cat, CatVote, User, Admin],
+              synchronize: true,
+            };
+
+            const options = {
+              host: POSTGRES_HOST,
+              port: POSTGRES_PORT,
+              username: POSTGRES_USER,
+              password: POSTGRES_PASSWORD,
+              database: POSTGRES_DB,
+            };
+            return { ...options, ...baseOptions } as TypeOrmModuleOptions;
+          },
+          inject: [RootConfig],
+        }),
+        TypeOrmModule.forFeature([Cat, CatVote, User, Admin]),
+      ],
       providers: [CatsService],
     }).compile();
 
     service = module.get<CatsService>(CatsService);
+    await service.onModuleInit();
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  afterAll(async () => {
+    await service['catRepository'].query(
+      'TRUNCATE TABLE admin, "user",cat, cat_vote CASCADE;',
+    );
   });
 
   describe('getTopFive', () => {
-    it('should return top 5 cats', async () => {
-      const result: CatDTO[] = [];
-      jest.spyOn(service, 'getTopFive').mockImplementation(async () => result);
-      expect(await service.getTopFive()).toBe(result);
+    it('should return top 5 cats based on votes', async () => {
+      const topCats = await service.getTopFive();
+
+      expect(topCats).toBeDefined();
+      expect(Array.isArray(topCats)).toBeTruthy();
+      expect(topCats.length).toBeLessThanOrEqual(5);
+      // Add more detailed assertions as needed
     });
   });
 
   describe('getPaginated', () => {
     it('should return paginated cats', async () => {
-      const paginationParams: PaginationParamsDTO = { page: 1, limit: 10 };
-      const result: PaginationResultDTO = {
-        items: [],
-        hasNext: false,
-        total: 0,
-        hasPrev: false,
-      };
-      jest
-        .spyOn(service, 'getPaginated')
-        .mockImplementation(async () => result);
-      expect(await service.getPaginated(paginationParams)).toBe(result);
+      // Assuming your seeding process includes creating some cats,
+      // adjust `expectedTotal` and `expectedPageCount` based on your seed data.
+      const expectedTotal = 1000; // Example total seeded cats
+      const page = 1;
+      const limit = 10;
+      const paginationParams = { page, limit };
+
+      const result = await service.getPaginated(paginationParams);
+
+      expect(result).toBeDefined();
+      expect(result.items.length).toBeLessThanOrEqual(limit);
+      expect(result.total).toBe(expectedTotal);
+      expect(result.hasNext).toBe(true);
+      expect(result.hasPrev).toBe(false);
+    });
+  });
+
+  describe('Voting', () => {
+    // Assuming there's at least one cat and one user seeded by your `seedData` method.
+    it('should allow a user to vote for a cat and then remove the vote', async () => {
+      const cat = await service['catRepository']
+        .createQueryBuilder('cat')
+        .select()
+        .orderBy('RANDOM()')
+        .getOne();
+      const user = await service['userRepository']
+        .createQueryBuilder('user')
+        .select()
+        .orderBy('RANDOM()')
+        .getOne();
+      const voteParams = { catId: cat.id, userId: user.id };
+
+      // Add a vote
+      const vote = await service.addVoteForCat(voteParams);
+      expect(vote).toBeDefined();
+      expect(vote.cat.id).toEqual(cat.id);
+      expect(vote.user.id).toEqual(user.id);
+
+      // Remove the vote
+      await service.removeVoteForCat(voteParams);
+      const removedVote = await service['voteRepository'].findOne({
+        where: { cat: { id: cat.id }, user: { id: user.id } },
+      });
+      expect(removedVote).toBeNull();
     });
   });
 
   describe('getById', () => {
-    it('should return a cat by id', async () => {
-      const catId = '1';
-      const result: CatDTO = mockCats.find((cat) => cat.id === catId) as CatDTO;
-      jest.spyOn(service, 'getById').mockImplementation(async () => result);
-      expect(await service.getById(catId)).toBe(result);
-    });
-  });
+    it('should return a cat when given a valid ID', async () => {
+      const existingCat = await service['catRepository'].findOneBy({});
+      if (!existingCat) {
+        throw new Error('No cats found in the database');
+      }
+      // Use the existingCatId fetched in the beforeAll hook
+      const cat = await service.getById(existingCat.id);
 
-  describe('addVoteForCat', () => {
-    it('should return confirmation of voting for a cat', async () => {
-      const voteParams = { catId: '1', userId: 'user123' };
-      const result = 'votedForCat';
-      jest.spyOn(service, 'addVoteForCat').mockImplementation(() => result);
-      expect(service.addVoteForCat(voteParams)).toBe(result);
+      expect(cat).toBeDefined();
+      expect(cat.id).toBe(existingCat.id);
     });
-  });
 
-  describe('removeVoteForCat', () => {
-    it('should return confirmation of removing vote for a cat', () => {
-      const voteParams = { catId: '1', userId: 'user123' };
-      const result = 'removedVoteForCat';
-      jest.spyOn(service, 'removeVoteForCat').mockImplementation(() => result);
-      expect(service.removeVoteForCat(voteParams)).toBe(result);
+    it('should return null or handle appropriately when given a non-existing ID', async () => {
+      const randomId = '123e4567-e89b-12d3-a456-426614174000';
+      const cat = await service.getById(randomId);
+
+      expect(cat).toBeNull();
     });
   });
 });
--- backend/src/cats/cats.service.ts
+++ backend/src/cats/cats.service.ts
@@ -1,101 +1,242 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import {
   CatDTO,
   PaginationParamsDTO,
   PaginationResultDTO,
 } from './dto/cats.index';
 import { IVoteForCatParams } from './types/cats.type';
-
-export const mockCats = [
-  {
-    id: '1',
-    name: 'Cat 1',
-    favoriteFood: 'Tuna',
-    birthday: new Date(),
-    age: 1,
-    location: 'Location 1',
-    furColor: 'White',
-    height: 30,
-    weight: 5,
-    imageUrl: 'https://example.com/cat.jpg',
-  },
-  {
-    id: '2',
-    name: 'Cat 2',
-    favoriteFood: 'Tuna',
-    birthday: new Date(),
-    age: 1,
-    location: 'Location 2',
-    furColor: 'White',
-    height: 30,
-    weight: 5,
-    imageUrl: 'https://example.com/cat.jpg',
-  },
-  {
-    id: '3',
-    name: 'Cat 3',
-    favoriteFood: 'Tuna',
-    birthday: new Date(),
-    age: 1,
-    location: 'Location 3',
-    furColor: 'White',
-    height: 30,
-    weight: 5,
-    imageUrl: 'https://example.com/cat.jpg',
-  },
-  {
-    id: '4',
-    name: 'Cat 4',
-    favoriteFood: 'Tuna',
-    birthday: new Date(),
-    age: 1,
-    location: 'Location 4',
-    furColor: 'White',
-    height: 30,
-    weight: 5,
-    imageUrl: 'https://example.com/cat.jpg',
-  },
-  {
-    id: '5',
-    name: 'Cat 5',
-    favoriteFood: 'Tuna',
-    birthday: new Date(),
-    age: 1,
-    location: 'Location 5',
-    furColor: 'White',
-    height: 30,
-    weight: 5,
-    imageUrl: 'https://example.com/cat.jpg',
-  },
-];
+import { RootConfig } from '@/config/env.validation';
+import { Repository } from 'typeorm';
+import { Cat, CatVote } from './cats.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { createRandomUser, createRandomCat } from '@/utils/fakerUtils';
+import { AuthService } from '@/auth/auth.service';
+import { IAdmin } from '@/auth/auth.types';
+import { User } from '@/auth/user.entity';
 
 @Injectable()
 export class CatsService {
+  constructor(
+    private readonly rootConfig: RootConfig,
+    @InjectRepository(Cat)
+    private catRepository: Repository<Cat>,
+    @InjectRepository(CatVote)
+    private voteRepository: Repository<CatVote>,
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+    private authService: AuthService,
+  ) {}
+
+  async onModuleInit(): Promise<void> {
+    await this.seedData();
+  }
+
+  private async seedData(): Promise<void> {
+    const start = performance.now();
+
+    const { NODE_ENV } = this.rootConfig;
+
+    const isProdEnv = NODE_ENV === 'production';
+    const isStagingEnv = NODE_ENV === 'staging';
+
+    if (isProdEnv || isStagingEnv) {
+      Logger.log(
+        'Seeding aborted production enviroment detected. ' + NODE_ENV,
+        { time: performance.now() - start },
+      );
+      return;
+    }
+
+    Logger.log('Seeding data...', { NODE_ENV });
+
+    try {
+      const catsCount = await this.catRepository.count();
+      if (catsCount > 0) {
+        Logger.log('Seeding aborted, cats table is not empty');
+        return;
+      }
+
+      const usersCount = await this.userRepository.count();
+      if (usersCount > 0) {
+        Logger.log('Seeding aborted, users table is not empty');
+        return;
+      }
+
+      const votesCount = await this.voteRepository.count();
+      if (votesCount > 0) {
+        Logger.log('Seeding aborted, votes table is not empty');
+        return;
+      }
+
+      Logger.log('Seeding kitties...');
+
+      const CAT_AMOUNT = 1000;
+      const randomCats = Array.from({ length: CAT_AMOUNT }, () =>
+        createRandomCat(),
+      );
+
+      await this.batchInsert(this.catRepository, randomCats, CAT_AMOUNT);
+
+      Logger.log('Seeding users...');
+
+      const USER_AMOUNT = 100;
+      const randomUsers = await Promise.all(
+        Array.from({ length: USER_AMOUNT }, () => createRandomUser()),
+      );
+      await this.batchInsert(this.userRepository, randomUsers, USER_AMOUNT);
+
+      Logger.log('Seeding votes...');
+
+      const cats = await this.catRepository.find();
+      const users = await this.userRepository.find();
+
+      const uniqueVotes = new Set();
+      const DIVIDER = 'vvvv';
+      while (uniqueVotes.size < 5000) {
+        uniqueVotes.add(
+          `${users[Math.floor(Math.random() * users.length)].id}${DIVIDER}${
+            cats[Math.floor(Math.random() * cats.length)].id
+          }`,
+        );
+      }
+
+      await Promise.all(
+        Array.from(uniqueVotes).map((vote: string) => {
+          const [userId, catId] = vote.split(DIVIDER);
+          return this.addVoteForCat({ catId, userId });
+        }),
+      );
+
+      Logger.log('Seeding admin...');
+
+      const admin = await createRandomUser();
+      admin.username = 'admin';
+      admin.password = 'admin';
+
+      admin['email'] = 'admin@admin.com';
+      admin['accessLevel'] = 'admin';
+      await this.authService.createAdmin(admin as IAdmin);
+    } catch (error) {
+      Logger.error('Seeding failed', error);
+      throw error;
+    }
+    Logger.log('Seeding succesful...', { time: performance.now() - start });
+
+    return;
+  }
+
+  private async batchInsert<T>(
+    repository: Repository<T>,
+    entities: T[],
+    batchSize: number,
+  ) {
+    for (let i = 0; i < entities.length; i += batchSize) {
+      const batch = entities.slice(i, i + batchSize);
+      await repository.save(batch);
+    }
+  }
+
   async getTopFive(): Promise<CatDTO[]> {
-    return mockCats;
+    const topCats = await this.catRepository
+      .createQueryBuilder('cat')
+      .leftJoin('cat_vote', 'cv', 'cv.cat_id = cat.id')
+      .select('cat')
+      .addSelect('COUNT(cv.id)', 'likes')
+      .groupBy('cat.id')
+      .orderBy('likes', 'DESC')
+      .limit(5)
+      .getRawMany();
+
+    return topCats.map((cat) => this.mapToDTO(cat));
   }
 
   async getPaginated({
     page,
     limit,
-  }: PaginationParamsDTO): Promise<PaginationResultDTO> {
+  }: PaginationParamsDTO): Promise<PaginationResultDTO<CatDTO>> {
+    const [items, total] = await this.catRepository.findAndCount({
+      skip: (page - 1) * limit,
+      take: limit,
+    });
+
+    const catDTOs = items.map((cat) => this.mapToDTO(cat));
+
+    const hasNext = total > page * limit;
+    const hasPrev = page > 1;
+
     return {
-      items: [],
-      hasNext: false,
-      total: 0,
-      hasPrev: false,
+      items: catDTOs,
+      hasNext,
+      total,
+      hasPrev,
     };
   }
 
-  async getById(id: string): Promise<CatDTO> {
-    return mockCats[0];
+  private removePrefix(cat: Cat) {
+    return Object.keys(cat).reduce((acc, key) => {
+      const newKey = key.replace('cat_', '');
+      acc[newKey] = cat[key];
+      if (newKey === 'likes') {
+        acc[newKey] = +cat[key];
+      }
+      return acc;
+    }, {}) as Cat;
   }
 
-  addVoteForCat({ catId, userId }: IVoteForCatParams) {
-    return 'votedForCat';
+  private mapToDTO(cat: Cat): CatDTO {
+    const fixedCat = this.removePrefix(cat);
+    return new CatDTO(fixedCat);
   }
 
-  removeVoteForCat({ catId, userId }: IVoteForCatParams) {
-    return 'votedForCat';
+  async getById(id: string): Promise<Cat> {
+    const cat = await this.catRepository.findOne({ where: { id } });
+    if (!cat) {
+      Logger.warn('Cat not found', id);
+    }
+    return cat;
+  }
+
+  async addVoteForCat({ catId, userId }: IVoteForCatParams) {
+    const voteExists = await this.voteRepository.findOneBy({
+      cat: { id: catId },
+      user: { id: userId },
+    });
+    if (voteExists) {
+      Logger.warn('Vote already exists');
+      return null;
+    }
+
+    const cat = await this.catRepository.findOneBy({ id: catId });
+    if (!cat) {
+      throw new Error('Cat not found');
+    }
+
+    const user = await this.authService.getUserById(userId);
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    const vote = new CatVote();
+    vote.cat = cat;
+    vote.user = user;
+
+    await this.voteRepository.save(vote);
+    return vote;
+  }
+
+  async removeVoteForCat({ catId, userId }: IVoteForCatParams) {
+    const voteExists = await this.voteRepository.findOneBy({
+      cat: { id: catId },
+      user: { id: userId },
+    });
+    if (!voteExists) {
+      Logger.warn('Vote not found', {
+        voteTodelete: voteExists,
+        catId,
+        userId,
+      });
+      return;
+    }
+    await this.voteRepository.remove(voteExists);
   }
 }
