dfd7cc8b318a490153d17de832624bbeef8f2d3f
Darkmift <darkmift@gmail.com>
2024-03-17 21:24:19 +0200

    Add cookie-parser and enable CORS for token authentication

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,22 +5,25 @@ import { RootConfig } from './config/env.validation';
 import { initSwagger } from './utils/swagger/config';
 import { DatadogLogger } from './utils/datadog/datadog';
 import AllExceptionsFilter from './common/filters/http-exception-filter';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const rootConfig = app.get(RootConfig);
   const { PORT, FRONTEND_ORIGIN } = rootConfig;
-  // const allowedOrigins = FRONTEND_ORIGIN.split(',');
+  const allowedOrigins = FRONTEND_ORIGIN.split(',');
 
   try {
     // Use DatadogLogger with the configuration
     const logger = new DatadogLogger(rootConfig);
     app.useLogger(logger);
 
+    // apply global exception filter
     const filter = new AllExceptionsFilter(logger);
     app.useGlobalFilters(filter);
 
+    // apply with class-validator
     app.useGlobalPipes(
       new ValidationPipe({
         whitelist: true,
@@ -29,7 +32,23 @@ async function bootstrap() {
       }),
     );
 
+    // swagger setup
     initSwagger(app);
+
+    // for cookie token authentication
+    app.enableCors({
+      origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+          callback(null, true);
+        } else {
+          callback(new Error('Not allowed by CORS'));
+        }
+      },
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      allowedHeaders: 'Content-Type, Accept',
+      credentials: true,
+    });
+    app.use(cookieParser());
 
     await app.listen(PORT);
     Logger.log(`Server started on port ${PORT}`);
