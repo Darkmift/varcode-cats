3b26e43def3d6ef42d3858217b3c4fcba148e44a
Darkmift <darkmift@gmail.com>
2024-03-18 00:35:53 +0200

    Add JWT module to CatsModule

--- backend/src/cats/cats.module.ts
+++ backend/src/cats/cats.module.ts
@@ -5,10 +5,23 @@ import { AuthModule } from '@/auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Cat, CatVote } from './cats.entity';
 import { User } from '@/auth/user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { TypedConfigModule } from 'nest-typed-config';
+import { RootConfig } from '@/config/env.validation';
 
 @Module({
   providers: [CatsService],
   controllers: [CatsController],
-  imports: [AuthModule, TypeOrmModule.forFeature([Cat, CatVote, User])],
+  imports: [
+    AuthModule,
+    TypeOrmModule.forFeature([Cat, CatVote, User]),
+    JwtModule.registerAsync({
+      imports: [TypedConfigModule],
+      useFactory: async (rootConfig: RootConfig) => ({
+        secret: rootConfig.JWT_SECRET,
+      }),
+      inject: [RootConfig],
+    }),
+  ],
 })
 export class CatsModule {}
