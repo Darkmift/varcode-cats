ee51219a289934395a23aad361345b6c91febd30
Darkmift <darkmift@gmail.com>
2024-03-18 17:28:39 +0200

    Add Redux store provider to main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,14 @@
+import '@/index.scss';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import { Provider as ReduxStoreProvider } from 'react-redux';
+import store from './store/index.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ReduxStoreProvider store={store}>
+      <App />
+    </ReduxStoreProvider>
   </React.StrictMode>
 );
