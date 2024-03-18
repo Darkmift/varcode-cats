88ddba8b211652eb1c8ea5d5a27861ec1324d9c6
Darkmift <darkmift@gmail.com>
2024-03-18 15:14:11 +0200

    Add extractUserDataFromRequest utility function

---
+++ backend/src/utils/extractUserDataFromRequest.ts
@@ -0,0 +1,12 @@
+import { JwtDecodedPayload } from '@/auth/auth.types';
+import { Request } from 'express';
+
+export default function extractUserDataFromRequest(
+  req: Request,
+): JwtDecodedPayload {
+  return {
+    id: req.user.id,
+    role: req.user.role,
+    username: req.user.username,
+  };
+}
