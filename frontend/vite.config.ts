688855f94897c725df07cdb5fb90d680d94576aa
Darkmift <darkmift@gmail.com>
2024-03-18 17:27:53 +0200

    Add aliases and paths to tsconfig.json and vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+const root = resolve(__dirname, 'src');
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': resolve(root, '/src'),
+    },
+  },
+});
