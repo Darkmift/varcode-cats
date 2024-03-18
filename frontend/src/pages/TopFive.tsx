34af33c7f9ed4fb5dc916f182c213625d917e6a2
Darkmift <darkmift@gmail.com>
2024-03-18 18:58:59 +0200

    Add search functionality to TopFive page

--- frontend/src/pages/TopFive.tsx
+++ frontend/src/pages/TopFive.tsx
@@ -1,11 +1,25 @@
 import PageWrapper from '@/components/common/PageWrapper';
+import SearchInput from '@/components/common/SearchInput';
+import { useAppDispatch, useAppSelector } from '@/store';
 import { Typography } from '@mui/material';
+import { setSearchTerm } from '@/store/slices/cats';
 type Props = {};
 
 function TopFive({}: Props) {
+  const searchTerm = useAppSelector((state) => state.cats.catNameSearchTerm);
+  const dispatch = useAppDispatch();
+
+  //a handler for search input
+
+  const handleSearchTerm = (term: string) => {
+    console.log('🚀 ~ handleSearchTerm ~ term:', term);
+    dispatch(setSearchTerm(term));
+  };
+
   return (
     <PageWrapper>
       <Typography variant="h4">TopFive</Typography>
+      <SearchInput emitNewTerm={handleSearchTerm} initialSearchTerm={searchTerm} />
     </PageWrapper>
   );
 }
